0c18aa1365033dd198a675da6e5272c1a796afc4
sdahal-dev <shubhamdahal3141@gmail.com>
2025-12-07 23:08:17 -0600

    Created Particle Effects On Pointer Drag

---
+++ script.js
@@ -0,0 +1,103 @@
+const canvas = document.getElementById("main-canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+const particlesArray = [];
+let isSlicing = false;
+const colors = [
+  "hsl(50, 100%, 90%)",  // white-yellow
+  "hsl(40, 100%, 60%)",  // gold
+  "hsla(29, 100%, 50%, 1.00)",  // orange-red
+];
+
+window.addEventListener("resize", () => {
+    canvas.width = innerWidth;
+    canvas.height = innerHeight;
+})
+
+const mouse = {
+    x: canvas.width/2,
+    y: canvas.height/2
+}
+
+canvas.addEventListener("pointerdown", (e) => {
+    isSlicing = true;
+    for(let i = 0; i < 15; i++){
+        particlesArray.push(new Particle());
+    }
+})
+
+canvas.addEventListener("pointermove", (e) => {
+    mouse.x = e.x;
+    mouse.y = e.y;
+    if(isSlicing){
+        for(let i = 0; i < 10; i++){
+            particlesArray.push(new Particle());
+        }
+    }
+})
+
+canvas.addEventListener("pointerup", () => {
+    isSlicing = false;
+})
+
+
+
+function drawCircle(){
+    ctx.fillStyle = "lightblue";
+    ctx.beginPath();
+    ctx.arc(mouse.x, mouse.y, 1, 0, Math.PI*2);
+    ctx.fill();
+}
+
+class Particle {
+    constructor(){
+        this.x = mouse.x;
+        this.y = mouse.y;
+        this.size = Math.random() * 3 + 1;
+        this.speedX = Math.random() * 6 - 3;
+        this.speedY = Math.random() * 6 - 3;
+        this.color = colors[Math.floor(Math.random() * colors.length)];
+        this.alpha = 1;
+    }
+    update(){
+        this.x += this.speedX;
+        this.y += this.speedY;
+        this.alpha -= 0.02;
+        if(this.size > 0.2) this.size -= 0.1;
+    }
+    draw() {
+        ctx.fillStyle = this.color;
+        ctx.globalAlpha = this.alpha;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI*2);
+        ctx.fill();
+        ctx.globalAlpha = 1;
+    }
+}
+
+function handleParticles(){
+    for(let i = 0; i < particlesArray.length; i++){
+        particlesArray[i].update();
+        particlesArray[i].draw();
+        if(particlesArray[i].size <= 0.3){
+            particlesArray.splice(i, 1);
+            i--;
+        }
+    }
+}
+console.log(particlesArray);
+function animate(){
+    ctx.fillStyle = "rgba(0,0,0,0.2)";
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    handleParticles();
+    drawCircle();
+    requestAnimationFrame(animate);
+}
+
+animate();
+
+
+
+
+
